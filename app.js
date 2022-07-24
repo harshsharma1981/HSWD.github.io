5c6568d2b972804da32977d341e2b182b6b6b898
harsh <109368583+harshsharma1981@users.noreply.github.com>
2022-07-24 12:11:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/static', express.static('static'));
 
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.get('/game', function(req, res) {
   res.sendFile(path.join(__dirname, 'views/game.html'));
